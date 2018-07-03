8b56d9b9f3f6cc843b0a6b1df891432fbebac74b
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2018-07-03 10:36:14 -0300

    Create Sena.js

---
+++ Sena.js
@@ -0,0 +1,24 @@
+class Sena{
+
+   constructor(qtdNumbers) {
+   		this.qtdNumbers = qtdNumbers > 60 ? 0 : qtdNumbers
+      this.numbers = []
+   }
+   
+	 getRandomNumber(){
+     let numberRandom = Math.floor(Math.random() * (60 - 1 + 1)) + 1
+
+    if(this.numbers.indexOf(numberRandom) < 0 ){
+      this.numbers.push(numberRandom)
+      this.numbers.sort((a, b) => a - b)
+    }
+
+    if(this.numbers.length < this.qtdNumbers){
+      this.getRandomNumber();
+    }
+  }
+}
+
+//let mega = new Sena(60);
+//mega.getRandomNumber();
+//document.write(mega.numbers.join(','))
