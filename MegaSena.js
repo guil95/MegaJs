401a8094f8152a4d971bb2eb6a228a75a1477c20
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2018-07-03 10:26:24 -0300

    Create MegaSena.js

---
+++ MegaSena.js
@@ -0,0 +1,23 @@
+class MegaSena{
+
+   constructor() {
+      this.numbers = []
+   }
+   
+	 getRandomNumber(){
+     let numberRandom = Math.floor(Math.random() * (60 - 1 + 1)) + 1
+
+    if(this.numbers.indexOf(numberRandom) < 0 ){
+      this.numbers.push(numberRandom)
+      this.numbers.sort((a, b) => a - b)
+    }
+
+    if(this.numbers.length < 6){
+      this.getRandomNumber();
+    }
+  }
+}
+
+let mega = new MegaSena();
+mega.getRandomNumber();
+console.log(mega.numbers)
