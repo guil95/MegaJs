3db365fb8def399005fd79d5a19178a1ef7ed20a
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2018-12-26 13:35:55 -0200

    Update MegaSena.js

--- MegaSena.js
+++ MegaSena.js
@@ -1,23 +1,27 @@
 class MegaSena{
 
    constructor() {
-      this.numbers = []
+      this.numbers = [];
    }
    
    getRandomNumber(){
-    let numberRandom = Math.floor(Math.random() * (60 - 1 + 1)) + 1
+    let numberRandom = Math.floor(Math.random() * (60 - 1 + 1)) + 1;
 
     if(this.numbers.indexOf(numberRandom) < 0 ){
-      this.numbers.push(numberRandom)
-      this.numbers.sort((a, b) => a - b)
+      this.addNumber(numberRandom);
     }
 
     if(this.numbers.length < 6){
       this.getRandomNumber();
     }
   }
+  
+  addNumber(numberRandom){
+    this.numbers.push(numberRandom);
+    this.numbers.sort((a, b) => a - b);
+  }
 }
 
-//let mega = new MegaSena();
-//mega.getRandomNumber();
-//console.log(mega.numbers)
+let mega = new MegaSena();
+mega.getRandomNumber();
+document.write(mega.numbers);
